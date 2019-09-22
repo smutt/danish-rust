d636041aa66931bb8e95085b26c843713033415c
Andrew McConachie <andrew@depht.com>
2019-09-22 14:40:32 -0400

    Continued working on valid-test.py

--- tools/valid-test.py
+++ tools/valid-test.py
@@ -42,18 +42,27 @@ def fetch_index(host):
 
 # https://stackoverflow.com/questions/7689941/how-can-i-retrieve-the-tls-ssl-peer-certificate-of-a-remote-host-using-python
 # TODO: Need to return the whole certificate chain and not just one
-def get_cert(host, port=443):
+def get_certs(host, port=443):
+  bad_host = False
   context = ssl.create_default_context()
+  context.verify_mode = ssl.CERT_REQUIRED
+  context.check_hostname = True
   conn = socket.create_connection((host, port))
   sock = context.wrap_socket(conn, server_hostname=host)
   sock.settimeout(HTTPS_TIMEOUT)
 
   try:
     der_cert = sock.getpeercert(True)
+  except:
+    bad_host = True
   finally:
     sock.close()
-  #return ssl.DER_cert_to_PEM_cert(der_cert)
-  return der_cert
+
+  if bad_host:
+    return False
+  else:
+    #return ssl.DER_cert_to_PEM_cert(der_cert)
+    return [der_cert]
 
 def get_tlsa(host, port=443, trans='tcp'):
   d = dns.resolver.Resolver()
@@ -67,16 +76,44 @@ def get_tlsa(host, port=443, trans='tcp'):
   else:
     rv = []
     for rr in resp.rrset:
-      rv.append(str(rr).strip('.'))
+      toks = str(rr).strip('.').split(' ')
+      rec = {}
+      rec['usage'] = int(toks[0])
+      rec['selector'] = int(toks[1])
+      rec['mtype'] = int(toks[2])
+      rec['data'] = toks[3].lower()
+      rv.append(rec)
     return rv
-    #return str(resp.rrset[0]).strip('.')
-    
-def validate(cert, tlsa, verbose=False):
+
+def dummy(x):
+  return x
+
+def validate(certs, tlsa_rrs, verbose=False):
   mTypes = {
+    0: dummy,
     1: hashlib.sha256,
     2: hashlib.sha512
   }
-  return True
+
+  for tlsa in tlsa_rrs:
+    if tlsa['selector'] == 1: # TODO: Implement this
+      print("TLSA Selector 1 currently unsupported")
+      exit(0)
+
+    if tlsa['usage'] == 0: # PKIX-TA, won't work without whole chain
+      if tlsa['data'] == mTypes[tlsa['mtype']](certs[0]).digest().hex():
+        return True
+    elif tlsa['usage'] == 1: # PKIX-EE
+      if tlsa['data'] == mTypes[tlsa['mtype']](certs[-1]).digest().hex():
+        return True
+    elif tlsa['usage'] == 2: # DANE-TA, won't work without whole chain
+      if tlsa['data'] == mTypes[tlsa['mtype']](certs[0]).digest().hex():
+        return True
+    elif tlsa['usage'] == 3: # DANE-EE
+      if tlsa['data'] == mTypes[tlsa['mtype']](certs[-1]).digest().hex():
+        return True
+
+  return False
 
   
 ###################
@@ -93,23 +130,27 @@ dom = args.domain[0].strip()
 if fetch_index(dom):
   if args.verbose:
     print("Success fetching https://" + dom + "/index.html")
-  
-  cert = get_cert(args.domain[0].strip())
-  tlsa_rrs = get_tlsa(args.domain[0].strip())
-  if tlsa_rrs:
-    if args.verbose:
-      print("TLSA RRs found for " + dom)
-      for rr in tlsa_rrs:
-        print(rr)
-    if validate(cert, tlsa_rrs):
-      print("Valid TLSA for " + dom)
-    else:
-      print("No valid TLSA for " + dom)
-
-  else:
-    print("No TLSA for " + dom)
-
 else:
   print("Failed fetching https://" + dom + "/index.html")
-  
+
+certs = get_certs(args.domain[0].strip())
+if certs == False:
+  print("No valid certificate found for " + dom)
+  sys.exit(0)
+
+tlsa_rrs = get_tlsa(args.domain[0].strip())
+if tlsa_rrs:
+  if args.verbose:
+    print("TLSA RRs found for " + dom)
+    for rr in tlsa_rrs:
+      print(rr)
+  if validate(certs, tlsa_rrs):
+    print("Valid TLSA for " + dom)
+  else:
+    print("No valid TLSA for " + dom)
+
+else:
+  print("No TLSA for " + dom)
+
+
 sys.exit(0)
